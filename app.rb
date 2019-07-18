750df801928085ae357cb509739701eef632a8b9
Sheena Bonilla <sheena.myb@gmail.com>
2019-07-18 04:44:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ module FormsLab
     post '/pirates' do
      @pirate = Pirate.new(params["pirate"])
      @ships = params["pirate"]["ships"]
+     @ships.each do |p|
      binding.pry
     end
   end
